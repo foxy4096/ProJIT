055cb28fbde6773df69d87eccdc2f2c0e0a07faf
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-03-19 12:31:02 +0530

    Wrote some more shitty code

--- person database.py
+++ person database.py
@@ -1,6 +1,22 @@
 # Create A database for person
 
+import sqlite3
+
+conn = sqlite3.connect('person.db')
+c = conn.cursor("""CREATE TABLE IF NOT EXISTS person (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    phone TEXT,
+    greetings INTEGER DEFAULT 0,
+    friends INTEGER DEFAULT 0
+);
+""")
+
+
 # A simple Person Class
+
+
 class Person:
     """
     A Class for Person
@@ -11,6 +27,12 @@ class Person:
         self.phone = phone
         self.friends = []
         self.greeting_count = 0
+
+        c.conn.execute("""
+        INSERT INTO person (name, email, phone)
+        VALUES (?, ?, ?)
+        """, (self.name, self.email, self.phone))
+        conn.commit()
 
     def __repr__(self):
         return f'{self.name} (email: {self.email}, phone: {self.phone})'
@@ -32,6 +54,9 @@ class Person:
     def __str__(self):
         return f'{self.name} (email: {self.email}, phone: {self.phone})'
 
+    def __repr__(self) -> str:
+        return f'{self.name} (email: {self.email}, phone: {self.phone})'
+
 def menu():
     """
     Prints the menu
@@ -46,6 +71,18 @@ def menu():
     print('7 - Print number of friends')
     print('8 - Exit')
 
-optitons = ['1', '2', '3', '4', '5', '6', '7', '8']
+options = ['1', '2', '3', '4', '5', '6', '7', '8']
 menu()
 input_option = input('Enter an option: ')
+
+if input_option not in options:
+    print('Invalid option')
+    exit()
+
+else:
+    if input_option == '1':
+        name = input('Enter name: ')
+        email = input('Enter email: ')
+        phone = input('Enter phone: ')
+        person = Person(name, email, phone)
+        print(person)
